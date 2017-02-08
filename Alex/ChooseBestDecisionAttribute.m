4a61be43d08b60934292fac745f3b890e2ad63ab
Alexandre Camuto <alexandrecamuto@MacBook-Pro-de-Alexandre.local>
2017-02-08 11:27:01 +0000

    Fixed p0 n0 n1 p1 definition

--- Alex/ChooseBestDecisionAttribute.m
+++ Alex/ChooseBestDecisionAttribute.m
@@ -7,16 +7,12 @@ n = sum(binary_targets==0);
    for i=1:numel(attributes)
                   
         indices = (examples(:,attributes{i}) == 1); 
-        pos=0; 
-        pos = sum(binary_targets(indices)); 
-        p1 = pos/numel(indices); 
-        n1 = 1-p1; 
+        p1 = sum(binary_targets(indices)); 
+        n1 = numel(indices) - p1;
         
         indices = examples(:,attributes{i}) == 0; 
-        pos=0;
-        pos = sum(binary_targets(indices)); 
-        p0 = pos/numel(indices); 
-        n0 = 1-p0; 
+        p0 = sum(binary_targets(indices)); 
+        n0 = numel(indices)-p0; 
         
        Remainder = (p0+n0)*Inp(p0,n0)/(p+n)+(p1+n1)*Inp(p1,n1)/(p+n); 
        Gain = Inp(n,p) - Remainder; 
