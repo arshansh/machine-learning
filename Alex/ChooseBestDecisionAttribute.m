6656acd712bc707aec74a9ec5c6e7c002454086f
Alexandre Camuto <alexandrecamuto@MacBook-Pro-de-Alexandre.local>
2017-02-08 13:25:29 +0000

    even better

--- Alex/ChooseBestDecisionAttribute.m
+++ Alex/ChooseBestDecisionAttribute.m
@@ -6,18 +6,18 @@ n = sum(binary_targets==0);
  
    for i=1:numel(attributes)
                   
-        indices = (examples(:,attributes{i}) == 1); 
+        indices = find(examples(:,attributes{i}) == 1); 
         p1 = sum(binary_targets(indices)); 
         n1 = length(indices) - p1;
         
-        indices = examples(:,attributes{i}) == 0; 
-        p0 = sum(binary_targets(indices)); 
-        n0 = length(indices)-p0; 
+        indices2 = find(examples(:,attributes{i}) == 0); 
+        p0 = sum(binary_targets(indices2)); 
+        n0 = length(indices2)-p0; 
        
         p0n0 = (p0+n0)/length(binary_targets);
         p1n1 = (p1+n1)/length(binary_targets);
         
-       Remainder = p0n0*Inp(p0,n0)+p1n1*Inp(p1,n1)/(p+n); 
+       Remainder = p0n0*Inp(p0,n0)+p1n1*Inp(p1,n1)/(length(binary_targets)); 
        Gain = Inp(n,p) - Remainder; 
        if ~exist('previous_gain','var')
            previous_gain=Gain; 
