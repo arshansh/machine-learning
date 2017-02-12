2ab6ab9e4c4292fbc09e6bddb6a42b99bc23f670
Alexandre Camuto <alexandrecamuto@MacBook-Pro-de-Alexandre.local>
2017-02-12 23:01:30 +0000

    have testTrees resolve ambiguity
    have testTrees resolve ambiguity

--- Alex/DecisionScript.m
+++ Alex/DecisionScript.m
@@ -21,7 +21,7 @@ end
 confused = zeros(6,6,10);
 notwrong = zeros(10,1); 
 wrong = zeros(10,1); 
-predictions = zeros(100,6,10); 
+predictions = zeros(100,1,10); 
 
 for i=1:10
     
@@ -39,45 +39,21 @@ for i=1:10
         trees(j,:) = DECISIONTREELEARNING(train_x,attributes,y_emotion(j,:));
     end 
     
-    predictions(:,:,i) = testTrees(trees,test_x); 
+    predictions(:,i) = testTrees(trees,test_x); 
     
 
     prob = zeros(1,6); 
     
     for l=1:length(test_y(:,i))
      
-        if (~any(predictions(l,:,i))) 
-             predictions(l,1+round(rand*5),i)=1;   %even probability
-%           for j=1:6
-%          prob(j) = sum(y_emotion(j,:))/length(y_emotion(j,:)); 
-%           end
-%           dist_rand = sum(rand >= cumsum([0, prob]));
-%          predictions(l,dist_rand,i)=1;   % probability given by the training data
 
-        elseif (length(find(predictions(l,:,i)==1))>1)
-            
-            indices = find(predictions(l,:,i)==1); 
-            
-            total = 0; 
-            for m=1:length(indices)
-                total = total + sum(y_emotion(m,:)); 
-            end
-            for m=1:length(indices)
-            prob2(m) = sum(y_emotion(m,:))/total; 
-            end
-            split=round(1+rand(size(indices)-1));  %even prob    
-%             split = sum(rand >= cumsum([0, prob2])); 
-            indices(split) = [];         
-            predictions(l,indices,i) = 0; 
-        end 
-            
-        if(predictions(l,test_y(l,i),i))
+        if(predictions(l,i)==test_y(l,i)); 
             notwrong(i) = notwrong(i) +1; 
             confused(test_y(l,i),test_y(l,i),i) = confused(test_y(l,i),test_y(l,i)) +1; 
             
         else
             wrong(i) = wrong(i) +1; 
-            confused(find(predictions(l,:,i)==1),test_y(l,i),i) = confused(find(predictions(l,:,i)==1),test_y(l,i),i)+1;  
+            confused(find(predictions(l,i)==1),test_y(l,i),i) = confused(find(predictions(l,i)==1),test_y(l,i),i)+1;  
         end 
         
     end 
@@ -103,7 +79,7 @@ for i=1:10
     
     
     for j = 1:100
-    output(j+(i-1)*100,:) = predictions(j,:,i)==1; 
+    output(j+(i-1)*100,predictions(j,i)) = 1; 
     targets(j+(i-1)*100,test_y(j,i)) = 1; 
     
     end
--- Alex/testTrees.m
+++ Alex/testTrees.m
@@ -1,6 +1,7 @@
-function predictions=testTrees(T,x2)
+function predictions_enumerated=testTrees(T,x2)
 
 predictions = zeros(length(x2),length(T)); 
+predictions_enumerated = zeros(length(x2),1);
 for i=1:length(x2)
         
     for j=1:length(T)
@@ -14,7 +15,39 @@ for i=1:length(x2)
     end 
     
     end
+    
+end
 
+for i=1:length(x2)
+
+if (~any(predictions(i,:))) 
+%              predictions(l,1+round(rand*5),i)=1;   %even probability
+          for j=1:6
+         prob(j) = sum(predictions(:,j))/sum(sum(predictions(:,:))); 
+          end
+          dist_rand = sum(rand >= cumsum([0, prob]));
+         predictions(i,dist_rand)=1;   % probability given by the training data
+
+        elseif (length(find(predictions(i,:)==1))>1)
+            
+            indices = find(predictions(i,:)==1); 
+            
+            total = 0; 
+            for m=1:length(indices)
+                total = total + sum(predictions(:,m)); 
+            end
+            for m=1:length(indices)
+            prob2(m) = sum(predictions(:,m))/total; 
+            end
+%             split=round(1+rand(size(indices)-1));  %even prob    
+            split = sum(rand >= cumsum([0, prob2])); 
+            indices(split) = [];         
+            predictions(i,indices) = 0; 
+    end 
+
+
+
+ predictions_enumerated(i)= find(predictions(i,:)==1); 
 end
 
 end 
